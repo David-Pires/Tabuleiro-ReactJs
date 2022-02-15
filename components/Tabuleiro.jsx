58a367a2c5650508c10686a4bf3518fe7979d538
David Pires <developer.david@outlook.com>
2022-02-15 15:48:03 -0300

    Application of components using ReactJS

---
+++ components/Tabuleiro.jsx
@@ -0,0 +1,19 @@
+import Linha from './Linha';
+
+export default function Tabuleiro(props) {
+  return (
+    <div>
+      <Linha/>
+      <Linha preta/>
+      <Linha/>
+      <Linha preta/>
+      <Linha/>
+      <Linha preta/>
+      <Linha/>
+      <Linha preta/>
+
+
+
+    </div>
+  )
+}
